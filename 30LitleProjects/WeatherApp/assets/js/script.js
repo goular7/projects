a613801e77dd9ff5e037b9d86dc1cf85a1971134
goular7 <gustavogoulart1970@gmail.com.br>
2023-12-22 13:55:05 -0300

    addCod

---
+++ 30LitleProjects/WeatherApp/assets/js/script.js
@@ -0,0 +1,7 @@
+const apiKey = "5c44e0579ec67e6e3222adcc4b2910ff";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+}
